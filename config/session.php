490717a59e256ed32d1ec9ff55ecbc66fa5fb5b5
leftus <1399301831@qq.com>
2018-02-13 22:14:19 +0800

    'data'

--- config/session.php
+++ config/session.php
@@ -148,7 +148,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', "shop.xunibaike.com"),
+    'domain' => env('SESSION_DOMAIN', null),
 
     /*
     |--------------------------------------------------------------------------
