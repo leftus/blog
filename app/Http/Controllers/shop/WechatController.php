afbd7e05e0d14a85f790dcce970f896b330580dc
leftus <1399301831@qq.com>
2018-02-13 11:36:27 +0800

    openid

--- app/Http/Controllers/shop/WechatController.php
+++ app/Http/Controllers/shop/WechatController.php
@@ -11,7 +11,11 @@ class WechatController extends Controller{
     if($code){
       $token_url="https://api.weixin.qq.com/sns/oauth2/access_token?appid=".config('wechat.mp.appid')."&secret=".config('wechat.mp.appsecret')."&code=".$code."&grant_type=authorization_code";
       $res = file_get_contents($token_url);
-      var_dump($res);
+      $obj=json_decode($res);
+      if($obj->access_token){
+        session('openid',$obj->openid);
+        return redirect('/');
+      }
     }else{
       session(['state' => 'nnLGhnYJxkf4H3uMMf4h5O4v3l2NGnU4wGlUrcxiwSg']);
       $code_url="https://open.weixin.qq.com/connect/oauth2/authorize?appid=".config('wechat.mp.appid')."&redirect_uri=".urlencode('http://shop.xunibaike.com/wechat')."&response_type=code&scope=snsapi_userinfo&state=".session('state')."#wechat_redirect";
