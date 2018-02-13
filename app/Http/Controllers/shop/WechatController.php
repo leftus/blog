7682cfe1412c209ca7604e97225617d9b8734b9b
leftus <1399301831@qq.com>
2018-02-13 10:43:39 +0800

    map

---
+++ app/Http/Controllers/shop/WechatController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\shop;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class WechatController extends Controller{
+  function index(){
+    
+  }
+}
